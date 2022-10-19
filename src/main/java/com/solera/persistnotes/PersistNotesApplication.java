77fda810027f89867b09c17b6e5ccb0904068afc
Marquez Francisco <francisco.marquez@solera.com>
2022-10-19 09:43:28 +0200

    Spring Boot Files

---
+++ src/main/java/com/solera/persistnotes/PersistNotesApplication.java
@@ -0,0 +1,13 @@
+package com.solera.persistnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistNotesApplication.class, args);
+	}
+
+}
